354293473cd4596ca3d149dfd69f581c844c1ac2
Nader T <nadertux@gmail.com>
2013-07-05 17:21:55 +0100

    basic models + reports_dashboard and report_submission apps init

--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    slug = models.CharField(max_length=100)
+    defintion = models.CharField(max_length=300)
+
+class Feature(models.Model):
+    slug = models.CharField(max_length=200)
+    defintion = models.CharField(max_length=300)
+
+class Media(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    url = models.CharField(max_length=300)
+    external_url = models.CharField(max_length=300)
+
+
+class Report(models.Model):
+    # ...
+    pub_date = models.DateTimeField('date published')
+    category = models.ForeignKey(Category)
+    feature = models.ForeignKey(Feature)
+    media = models.ForeignKey(Media)
+
