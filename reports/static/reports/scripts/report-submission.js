b8691c18c76d1131a628c08a7c69c6459037ff5d
O-nixus <o-nixus@onixus-G74Sx.(none)>
2013-07-12 15:38:17 +0100

    switch to mapbox

--- reports/static/reports/scripts/report-submission.js
+++ reports/static/reports/scripts/report-submission.js
@@ -53,3 +53,50 @@ app.controller('ReportsMapCtrl', function($scope, $timeout) {
 		}
 	});
 });
+
+
+!(function() {
+
+	var marker = null,
+		geocoder = L.mapbox.geocoder('examples.map-4l7djmvo'),
+		map = L.mapbox.map('map', 'examples.map-4l7djmvo')
+		.addControl(L.mapbox.geocoderControl('examples.map-4l7djmvo')); geocoder.query('Tunisia', showMap);
+
+	function showMap(err, data) {
+		map.fitBounds(data.lbounds);
+	}
+
+	function DragPos(e) {
+		document.getElementById('loc').value = '' + e.latlng.lat + ':' + e.latlng.lng;;
+	}
+
+	function onMapClick(e) {
+		console.log(e.latlng);
+		// alert("You clicked the map at " + e.latlng);
+		if (!marker) {
+			marker = L.marker(e.latlng, {
+				icon: L.mapbox.marker.icon({
+					'marker-color': 'CC0033'
+				}),
+				draggable: true
+			});
+			marker.addTo(map);
+		} else {
+			console.log(marker);
+			marker.setLatLng(e.latlng);
+
+		}
+		// document.getElementById('loc').value = '' + e.latlng.lat + ':' + e.latlng.lng;
+		document.getElementById('id_location').value = e.latlng;
+		marker.on('dragend', function(e) {
+			window.eee = e;
+			document.getElementById('id_location').value = e.target._latlng;
+		});
+		//document.getElementById('loc').value = e.latlng;
+
+
+	}
+
+	map.on('click ', onMapClick);
+
+})();
