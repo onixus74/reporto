5031063597885caeeb85290599a7b3f1fefd888a
Nader T <nadertux@gmail.com>
2013-07-12 14:44:33 +0100

    cleanup

--- reports/admin.py
+++ reports/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from reports.models import Category, Feature, Media, Report
+from reports.models import Category, Feature, Media, Victim, Report
 
 admin.site.register(Category)
 admin.site.register(Feature)
 admin.site.register(Media)
+admin.site.register(Victim)
 admin.site.register(Report)
