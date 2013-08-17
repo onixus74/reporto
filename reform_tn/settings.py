00f5f00431ce14c248363e0a3c77cadb91d43caa
Nader T <nadertux@gmail.com>
2013-08-18 00:05:23 +0100

    deploy

--- reform_tn/settings.py
+++ reform_tn/settings.py
@@ -14,21 +14,26 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'reform_tn/development.sqlite3', # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        #'USER': '',
-        #'PASSWORD': '',
-        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
-    }
-}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn']
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': 'reform_tn/development.sqlite3', # Or path to database file if using sqlite3.
+#         # The following settings are not used with sqlite3:
+#         #'USER': '',
+#         #'PASSWORD': '',
+#         #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+#         #'PORT': '',                      # Set to empty string for default.
+#     }
+# }
+
+# # Hosts/domain names that are valid for this site; required if DEBUG is False
+# # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+# ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn']
+
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -97,21 +102,29 @@ MEDIA_ROOT = os.path.join(PROJECT_DIR,'media/')
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR,'static/')
+# # Absolute path to the directory static files should be collected to.
+# # Don't put anything in this directory yourself; store your static files
+# # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# # Example: "/var/www/example.com/static/"
+# STATIC_ROOT = os.path.join(PROJECT_DIR,'static/')
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+# # URL prefix for static files.
+# # Example: "http://example.com/static/", "http://static.example.com/"
+# STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
+# # Additional locations of static files
+# STATICFILES_DIRS = (
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+# )
+
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -281,3 +294,6 @@ YOUTUBE_CLIENT_ID = 'client-id'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 FILE_UPLOAD_MAX_MEMORY_SIZE = -1
+
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
