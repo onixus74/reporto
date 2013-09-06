f74d8acfd0dca8ee30338f361cd1729eae342afe
Nader T <nadertux@gmail.com>
2013-09-06 18:35:46 +0100

    fix

--- reform_tn/settings.py
+++ reform_tn/settings.py
@@ -26,7 +26,7 @@ DATABASES['default'] = dj_database_url.config()
 
 # # Hosts/domain names that are valid for this site; required if DEBUG is False
 # # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn', 'http://reform-tn-platform.herokuapp.com']
+# ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn', 'reform-tn-platform.herokuapp.com']
 ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
