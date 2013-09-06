91b532e49c0fe0e3241e6f1f62aca5ab3dc96162
Nader T <nadertux@gmail.com>
2013-09-06 17:04:35 +0100

    fix

--- reform_tn/settings.py
+++ reform_tn/settings.py
@@ -14,21 +14,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'reform_tn/development.sqlite3', # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        #'USER': '',
-        #'PASSWORD': '',
-        #'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        #'PORT': '',                      # Set to empty string for default.
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'reform_tn/development.sqlite3',
+#     }
+# }
+DATABASES = {}
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn']
+# # Hosts/domain names that are valid for this site; required if DEBUG is False
+# # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+# ALLOWED_HOSTS = ['localhost', 'reform.tn', 'www.reform.tn', 'http://reform-tn-platform.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -285,6 +284,4 @@ THUMBNAIL_ALIASES = {
     },
 }
 
-MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
-
-FILE_UPLOAD_MAX_MEMORY_SIZE = -1
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
